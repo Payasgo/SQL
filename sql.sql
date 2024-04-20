6d63d3c77f15ada4e7efa768922d5ce0558aed34
payasgo <payasgo55@gmail.com>
2024-04-20 08:53:26 +0530

    qurey

---
+++ sql.sql
@@ -0,0 +1,11 @@
+﻿SELECT * FROM dbo.Employee
+
+--- TO DELETE The Duplicate value in sql.
+;with cte as
+(
+SELECT *,
+ROW_NUMBER() OVER(PARTITION BY FirstName ORDER BY Phone) as dup
+FROM dbo.Employee
+)
+Delete FROM cte where dup >1
+
